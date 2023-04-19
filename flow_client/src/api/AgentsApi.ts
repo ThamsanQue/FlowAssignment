fc7d48e57fe9567040167704851c8f84acd5f4f6
Thamsanqa J <thamsanqa.dev@gmail>
2023-04-19 12:14:58 +0200

    api route changes & deployment config

--- flow_client/src/api/AgentsApi.ts
+++ flow_client/src/api/AgentsApi.ts
@@ -5,7 +5,7 @@ import axios from "axios"
 export const AgentsApi = {
     getAllAgents: async () => {
         try {
-            return await axios.get('http://localhost:3000/agents');
+            return await axios.get('https://flow-server-ruby.vercel.app/agents');
 
         } catch (error) {
             console.error(error)
@@ -14,7 +14,7 @@ export const AgentsApi = {
 
     getAgentById: async (id: string | undefined) => {
         try {
-            return await axios.get(`http://localhost:3000/agents/${id}`);
+            return await axios.get(`https://flow-server-ruby.vercel.app/agents/${id}`);
         } catch (error) {
             console.error(error)
         }
--- flow_client/src/api/ListingsApi.ts
+++ flow_client/src/api/ListingsApi.ts
@@ -4,14 +4,14 @@ import axios from "axios"
 export const ListingApi = {
     getListingsByAgentId: async (id: string | undefined) => {
         try {
-            return await axios.get(`http://localhost:3000/listings/agent/${id}`);
+            return await axios.get(`https://flow-server-ruby.vercel.app/listings/agent/${id}`);
         } catch (error) {
             console.error(error)
         }
     },
     getListingById: async (id: string | undefined) => {
         try {
-            return await axios.get(`http://localhost:3000/listings/${id}`);
+            return await axios.get(`https://flow-server-ruby.vercel.app/listings/${id}`);
         } catch (error) {
             console.error(error)
         }
